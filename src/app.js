0498eaa1326a29792f69cf4d27019f0b7ea9a7c2
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-03-14 10:07:44 +0900

    use response_url

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { WebClient } = require('@slack/client');
 const usecase = require('./usecase.js');
+const util = require('./util.js');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/lot', (req, res) => {
   if (req.body.token !== process.env.VERIFICATION_TOKEN) res.status(403).send();
+  const channel = req.body.channel_id;
+  const url = req.body.response_url;
+  res.send();
 
-  const web = new WebClient(process.env.OAUTH_TOKEN);
-  web.channels.info({channel: req.body.channel_id})
-    .then(channel => res.send(usecase.createMessage(channel.channel.members)));
+  usecase.channelMembers(channel)
+    .then(members => util.random(members))
+    .then(member => ({
+        response_type: 'in_channel',
+        text: `<@${member}> chosed!`
+      }))
+    .then(message => usecase.postJSON(url, message));
 });
 
 app.listen(process.env.PORT, () => console.log('App started'));
--- src/usecase.js
+++ src/usecase.js
@@ -1,9 +1,19 @@
-module.exports = { createMessage };
-const util = require('./util.js');
+module.exports = { channelMembers, postJSON };
 
-function createMessage(channelMembers) {
-  return {
-    response_type: 'in_channel',
-    text: `<@${util.random(channelMembers)}> chosed!`
-  };
+const { WebClient } = require('@slack/client');
+const retry = require('retry');
+const request = require('request-promise-native');
+
+function channelMembers(channel) {
+  const token = process.env.OAUTH_TOKEN;
+  const retryConfig = retry.operation({ retries: 5 });
+  return new WebClient(token, { retryConfig }).channels.info({ channel })
+    .then(res => res.channel.members)
+    .catch(() => []);
+}
+
+function postJSON(uri, body) {
+  console.log(body);
+  return request({ method: 'POST', uri, body, json: true })
+    .then(console.log('send'));
 }
