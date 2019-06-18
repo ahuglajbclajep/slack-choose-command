073b9a5ba79737304ea5b85b6bad644a55769ae3
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-06-18 15:35:46 +0900

    fix declares

--- src/declares.d.ts
+++ src/declares.d.ts
@@ -1,12 +1,11 @@
-type ExpressRequest = import("express").Request;
-interface SlashCommandRequest extends ExpressRequest {
+type SlashCommandRequest = Omit<import("express").Request, "body"> & {
   body: {
     token: string;
     response_url: string;
     text: string;
     channel_id: string;
   };
-}
+};
 
 type Reply = (
   | { text: string }
